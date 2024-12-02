e9f04715bdc329c3a7d432dae3e604fb59114d68
Sai Kumar Kindigeri <132796829+saikumarkindigeriDev@users.noreply.github.com>
2024-12-02 14:14:27 +0530

    added shadow effect to header

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -45,7 +45,7 @@ export default function Header() {
     navigate("/")
   }
   return (
-    <header className="fixed top-0 left-0 w-full bg-white text-white p-2 ">
+    <header className="fixed top-0 left-0 w-full bg-white text-white p-2 shadow-md fixed w-full top-0 z-50">
       <nav aria-label="Global" className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8">
         <div className="flex lg:flex-1">
           <a href="#" className="-m-1.5 p-1.5">
